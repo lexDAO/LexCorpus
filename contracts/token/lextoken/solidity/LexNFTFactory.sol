c474967e598cf6b7fcd68f3640bf410e64043c81
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-09-14 10:02:50 -0400

    Update LexNFTFactory.sol

--- contracts/token/lextoken/solidity/LexNFTFactory.sol
+++ contracts/token/lextoken/solidity/LexNFTFactory.sol
@@ -1,19 +1,6 @@
 pragma solidity 0.5.17;
 
-library Address { // helper for address type - see openzeppelin-contracts/blob/master/contracts/utils/Address.sol
-    function isContract(address account) internal view returns (bool) {
-        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
-        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
-        // for accounts without code, i.e. `keccak256('')`
-        bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
-        assembly { codehash := extcodehash(account) }
-        return (codehash != accountHash && codehash != 0x0);
-    }
-}
-
 contract LexNFT {
-    using Address for address;
     address public owner;
     address public resolver;
     string public name;
@@ -81,6 +68,20 @@ contract LexNFT {
         emit Transfer(address(0), owner, totalSupply);
     }
     
+    /***************
+    HELPER FUNCTIONS
+    ***************/
+    function _callOptionalReturn(address recipient, bytes memory data) internal {
+        require(isContract(recipient), "SafeERC20: call to non-contract");
+
+        (bool success, bytes memory returnData) = recipient.call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returnData.length > 0) { // return data is optional
+            require(abi.decode(returnData, (bool)), "SafeERC20: erc20 operation did not succeed");
+        }
+    }
+    
     function _initReentrancyGuard() internal {
         _notEntered = true;
     }
@@ -95,17 +96,6 @@ contract LexNFT {
         return (codehash != accountHash && codehash != 0x0);
     }
     
-    function _callOptionalReturn(address recipient, bytes memory data) internal {
-        require(recipient.isContract(), "SafeERC20: call to non-contract");
-
-        (bool success, bytes memory returnData) = recipient.call(data);
-        require(success, "SafeERC20: low-level call failed");
-
-        if (returnData.length > 0) { // return data is optional
-            require(abi.decode(returnData, (bool)), "SafeERC20: erc20 operation did not succeed");
-        }
-    }
-    
     /************
     TKN FUNCTIONS
     ************/
@@ -116,6 +106,7 @@ contract LexNFT {
         getApproved[tokenId] = spender;
         
         emit Approval(msg.sender, spender, tokenId); 
+        
         return true;
     }
     
@@ -123,6 +114,7 @@ contract LexNFT {
         isApprovedForAll[msg.sender][spender] = approved;
         
         emit ApprovalForAll(msg.sender, spender, approved);
+        
         return true;
     }
 
@@ -188,14 +180,14 @@ contract LexNFT {
         return true;
     }
     
-    function safeTransferFrom(address sender, address recipient, uint256 tokenId, bytes memory data) public {
-        _callOptionalReturn(recipient, data);
-        transferFrom(sender, recipient, tokenId);
-    }
-    
     function safeTransferFrom(address sender, address recipient, uint256 tokenId) external {
         safeTransferFrom(sender, recipient, tokenId, "");
 
+    }
+    
+    function safeTransferFrom(address sender, address recipient, uint256 tokenId, bytes memory data) public {
+        _callOptionalReturn(recipient, data);
+        transferFrom(sender, recipient, tokenId);
     }
     
     /**************
