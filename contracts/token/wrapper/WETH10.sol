b7d15baf2f31a361bee0992b7e9d35e21f929b97
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-10-09 14:16:08 -0400

    Update WETH10.sol
    conform syntax

--- contracts/token/wrapper/WETH10.sol
+++ contracts/token/wrapper/WETH10.sol
@@ -5,7 +5,7 @@ contract WETH10 {
     string public name;
     string public symbol;
     uint8  public decimals;
-    bytes32 public PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public PERMIT_TYPEHASH = keccak256("Permit(address src,address guy,uint wad,uint nonce,uint deadline)");
     bytes32 public DOMAIN_SEPARATOR;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
