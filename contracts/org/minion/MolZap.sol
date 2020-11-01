e3c971890ca7f3fa7fdf757d67bb44eefd529995
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-11-01 14:27:55 -0500

    Update MolZap.sol

--- contracts/org/minion/MolZap.sol
+++ contracts/org/minion/MolZap.sol
@@ -46,17 +46,14 @@ library SafeMath { // arithmetic wrapper for unit under/overflow check
         if (a == 0) {
             return 0;
         }
-
         uint256 c = a * b;
         require(c / a == b);
-
         return c;
     }
     
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b > 0);
         uint256 c = a / b;
-
         return c;
     }
 }
