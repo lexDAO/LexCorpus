fbb9bf34884b2e803069de53b2b047a38cf0008c
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-12-23 16:49:23 -0500

    Update litetoken.sol

--- contracts/token/litetoken/litetoken.sol
+++ contracts/token/litetoken/litetoken.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.8.0; // SPDX-License-Identifier: GPL-3.0-or-later
-contract LiteToken {
+contract LiteToken { // minimal viable erc20 token with common extensions, such as burn, cap, mint, pauseable, admin functions
     address public owner;
     string public name;
     string public symbol;
