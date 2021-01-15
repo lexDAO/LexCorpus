056bc2a9e8bbb99f5775b43fc1f46b3652d75ca7
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-01-15 14:21:24 -0500

    Update LexList.sol
    add new lexlist nft

--- contracts/registry/lexlist/solidity/LexList.sol
+++ contracts/registry/lexlist/solidity/LexList.sol
@@ -11,89 +11,77 @@
 ██║     ██║╚════██║   ██║   
 ███████╗██║███████║   ██║   
 ╚══════╝╚═╝╚══════╝   ╚═╝*/
-pragma solidity 0.5.17;
+// Presented by LexDAO LLC
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.0;
 
 contract LexList {
-    address public governance;
-    address[] private listings;
-    string public message;
-    mapping(address => Contract) private contractList;
+    address public lexDAO;
+    uint256 public totalSupply;
+    string  public details;
+    string  public name;
+    string  public symbol;
     
-    event List(address indexed _contract);
-    event Delist(address indexed _contract);
-    event UpdateGovernance(address indexed governance);
-    event UpdateMessage(string indexed message);
+    mapping(address => uint256) public balanceOf;
+    mapping(uint256 => address) public ownerOf;
+    mapping(uint256 => string) public tokenURI;
+    mapping(bytes4 => bool) public supportsInterface; // eip-165 
     
-    struct Contract {
-        uint256 contractIndex;
-        bool listed;
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event UpdateGovernance(address indexed lexDAO, string details);
+    event UpdateTokenURI(uint256 indexed tokenId, string tokenURI);
+    
+    constructor(address _lexDAO, string memory _details, string memory _name, string memory _symbol) {
+        lexDAO = _lexDAO; 
+        details = _details; 
+        name = _name; 
+        symbol = _symbol;  
+        supportsInterface[0x80ac58cd] = true; // ERC721 
+        supportsInterface[0x5b5e139f] = true; // METADATA
     }
     
-    constructor (address[] memory _contract, address _governance, string memory _message) public {
-        for (uint256 i = 0; i < _contract.length; i++) {
-            contractList[_contract[i]].contractIndex = listings.push(_contract[i]) - 1;
-            contractList[_contract[i]].listed = true;
-        }
-        
-        governance = _governance;
-        message = _message;
-    }
-    
-    modifier onlyGovernance {
-        require(msg.sender == governance, "!governance");
+    modifier onlyLexDAO {
+        require(msg.sender == lexDAO, "!lexDAO");
         _;
     }
     
-    /****************
-    LISTING FUNCTIONS
-    ****************/
-    function delist(address[] calldata _contract) external onlyGovernance {
-        for (uint256 i = 0; i < _contract.length; i++) {
-            require(contractList[_contract[i]].listed, "!listed");
-            
-            uint256 contractToUnlist = contractList[_contract[i]].contractIndex;
-            address k = listings[listings.length - 1];
-            listings[contractToUnlist] = k;
-            contractList[k].contractIndex = contractToUnlist;
-            contractList[_contract[i]].listed = false;
-            listings.length--;
-            
-            emit Delist(_contract[i]);
+    function _mint(address to, string calldata _tokenURI) internal { 
+        totalSupply++;
+        uint256 tokenId = totalSupply;
+        balanceOf[to]++;
+        ownerOf[tokenId] = to;
+        tokenURI[tokenId] = _tokenURI;
+        emit Transfer(address(0), to, tokenId); 
+    }
+    
+    function mint(address to, string calldata _tokenURI) external onlyLexDAO { 
+        _mint(to, _tokenURI);
+    }
+    
+    function mintBatch(address[] calldata to, string[] calldata _tokenURI) external onlyLexDAO {
+        require(to.length == _tokenURI.length, "!to/tokenURI");
+        for (uint256 i = 0; i < to.length; i++) {
+            _mint(to[i], _tokenURI[i]); 
         }
     }
     
-    function list(address[] calldata _contract) external onlyGovernance { 
-        for (uint256 i = 0; i < _contract.length; i++) {
-            require(!contractList[_contract[i]].listed, "listed");
-            
-            contractList[_contract[i]].contractIndex = listings.push(_contract[i]) - 1;
-            contractList[_contract[i]].listed = true;
-            
-            emit List(_contract[i]);
-        }
+    function transfer(address from, address to, uint256 tokenId) external onlyLexDAO {
+        require(from == ownerOf[tokenId], "!owner");
+        balanceOf[from]--; 
+        balanceOf[to]++; 
+        ownerOf[tokenId] = to;
+        emit Transfer(from, to, tokenId); 
     }
     
-    function updateGovernance(address _governance) external onlyGovernance {
-        governance = _governance;
-        
-        emit UpdateGovernance(governance);
+    function updateGovernance(address _lexDAO, string calldata _details) external onlyLexDAO {
+        lexDAO = _lexDAO;
+        details = _details;
+        emit UpdateGovernance(_lexDAO, _details);
     }
     
-    function updateMessage(string calldata _message) external onlyGovernance {
-        message = _message;
-        
-        emit UpdateMessage(message);
-    }
-    
-    // *******
-    // GETTERS
-    // *******
-    function contractCount() external view returns (uint256) {
-        return listings.length;
-    }
-    
-    function isListed(address _contract) external view returns (bool) {
-        if(listings.length == 0) return false;
-        return (listings[contractList[_contract].contractIndex] == _contract);
+    function updateTokenURI(uint256 tokenId, string calldata _tokenURI) external onlyLexDAO {
+        require(tokenId <= totalSupply, "!exist");
+        tokenURI[tokenId] = _tokenURI;
+        emit UpdateTokenURI(tokenId, _tokenURI);
     }
 }
