4294b972f5e68155a8e0ae77f48f2723938a04da
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-01-20 21:47:05 -0500

    Update IERC20.sol

--- contracts/standard/token/ERC20/IERC20.sol
+++ contracts/standard/token/ERC20/IERC20.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.8.0;
 
-interface IERC20 { // erc20 token interface - see EIPs/blob/master/EIPS/eip-20.md
+interface IERC20 { // standard erc20 token interface
     function allowance(address owner, address spender) external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function totalSupply() external view returns (uint256);
