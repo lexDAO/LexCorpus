730cba73dfac280032f611c78a98fd3cfd0a2dbd
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-01-20 21:46:41 -0500

    Update Address.sol

--- contracts/standard/utils/Address.sol
+++ contracts/standard/utils/Address.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.8.0; 
 
-library Address { // helper for address type - see openzeppelin-contracts/blob/master/contracts/utils/Address.sol
+library Address { // helper for address type
     function isContract(address account) internal view returns (bool) {
         uint256 size;
         assembly { size := extcodesize(account) }
