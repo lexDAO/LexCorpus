3315d1ac0009fcc19616fb8af61aef098df9ff7b
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-01-20 21:31:43 -0500

    Create SafeERC20.sol

---
+++ contracts/standard/token/ERC20/SafeERC20.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.8.0;
+
+library SafeERC20 { // wrapper around erc20 token tx for non-standard contract - see openzeppelin-contracts/blob/master/contracts/token/ERC20/SafeERC20.sol
+    using Address for address;
+    
+    function safeTransfer(IERC20 token, address to, uint256 amount) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, amount));
+    }
+    
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 amount) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, amount));
+    }
+    
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+        (bool success, bytes memory returnData) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returnData.length > 0) { // return data is optional
+            require(abi.decode(returnData, (bool)), "SafeERC20: erc20 operation did not succeed");
+        }
+    }
+}
