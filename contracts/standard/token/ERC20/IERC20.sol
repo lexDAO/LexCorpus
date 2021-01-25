803c8f7c0d6cf7d01862826bb8c289fcca012f10
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-01-25 17:37:10 -0500

    Update IERC20.sol
    add events

--- contracts/standard/token/ERC20/IERC20.sol
+++ contracts/standard/token/ERC20/IERC20.sol
@@ -8,4 +8,6 @@ interface IERC20 { // standard erc20 token interface
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
 }
