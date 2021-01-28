dd19abf2dcf202bddb2800e997d6f3754c573d9c
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-01-28 18:38:06 -0500

    Create Owned.sol
    add owned contract pattern

---
+++ contracts/conditions/solidity/Owned.sol
@@ -0,0 +1,24 @@
+/// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+contract Owned {
+    address public owner; 
+    
+    /// @dev deploy Owned contract - `onlyOwner` modifer enforces condition
+    /// @param _owner account with `onlyOwner` permission 
+    constructor(address _owner) {
+        owner = _owner;
+    }
+    
+    /// @dev requires modified function to be called by `owner`
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    } 
+    
+    /// @dev update `owner` account
+    /// @param _owner account with `onlyOwner` permission 
+    function updateOwner(address _owner) external onlyOwner {
+        owner = _owner;
+    }
+}
