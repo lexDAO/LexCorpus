34e7351532a07b84ee7cbb745b7e80d58981d87b
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-01-29 11:45:26 -0500

    Update LexCert.sol
    simplify ierc20

--- contracts/registry/LexCert.sol
+++ contracts/registry/LexCert.sol
@@ -15,15 +15,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-interface IERC20 { // standard erc20 token interface
-    function allowance(address owner, address spender) external view returns (uint256);
+interface IERC20 { // brief erc20 token interface
     function balanceOf(address account) external view returns (uint256);
-    function totalSupply() external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address to, uint256 amount) external returns (bool);
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint256 amount);
-    event Transfer(address indexed from, address indexed to, uint256 amount);
 }
 
 contract LexList {
