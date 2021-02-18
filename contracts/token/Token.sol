3078779cf37631b74184b023b28dac073c5d6074
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-02-18 12:11:05 -0500

    Update and rename BaseToken.sol to Token.sol
    add events / clean ups

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -1,18 +1,25 @@
+/// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-contract BaseToken {
-    string public name = "TOKEN";
-    string public symbol = "TKN";
-    uint8 public decimals = 18;
-    uint256 public totalSupply = 100 ether;
-    
-    constructor() {balanceOf[msg.sender] = totalSupply;}
+contract Token {
+    string public name;
+    string public symbol;
+    uint8 constant public decimals = 18;
+    uint256 public totalSupply;
     
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
     
     event Transfer(address indexed from, address indexed to, uint256 amount);
     event Approval(address indexed owner, address indexed spender, uint256 amount);
+    
+    constructor(address owner, string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+        balanceOf[owner] = _totalSupply;
+        emit Transfer(address(0), owner, _totalSupply);
+    }
     
     function approve(address to, uint256 amount) external returns (bool) {
         allowance[msg.sender][to] = amount;
@@ -21,16 +28,16 @@ contract BaseToken {
     }
     
     function transfer(address to, uint256 amount) external returns (bool) {
-        balanceOf[msg.sender] = balanceOf[msg.sender] - amount;
-        balanceOf[to] = balanceOf[to] + amount;
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
         emit Transfer(msg.sender, to, amount);
         return true;
     }
     
     function transferFrom(address from, address to, uint256 amount) external returns (bool) {
-        balanceOf[from] = balanceOf[from] - amount;
-        balanceOf[to] = balanceOf[to] + amount;
-        allowance[from][msg.sender] = allowance[from][msg.sender] - amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        allowance[from][msg.sender] -= amount;
         emit Transfer(from, to, amount);
         return true;
     }
