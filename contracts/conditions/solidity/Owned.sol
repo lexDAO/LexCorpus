ffd21616449c0387030be4fe9d7ba6e21d4b2abc
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-02-18 11:47:44 -0500

    Update Owned.sol
    add pending owner stuff from https://github.com/boringcrypto/BoringSolidity/blob/master/contracts/BoringOwnable.sol

--- contracts/conditions/solidity/Owned.sol
+++ contracts/conditions/solidity/Owned.sol
@@ -1,24 +1,67 @@
 /// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-contract Owned {
-    address public owner; 
+contract Ownable {
+    address private _owner; // `owner` account with access control
+    address private _pendingOwner; // `pendingOwner` that can call `claimOwner()` for access control
     
-    /// @dev deploy Owned contract 
-    /// @param _owner account with `onlyOwner` permission 
-    constructor(address _owner) {
-        owner = _owner;
+    event TransferOwnership(address indexed from, address indexed to); // track `owner` transfers
+    event TransferOwnershipClaim(address indexed from, address indexed to); // track transfer of `owner` claim to `pendingOwner`
+    
+    /// @dev initialize contract
+    /// @param _owner Account granted access control
+    constructor(address owner) {
+        _owner = owner;
+        emit TransferOwnership(address(0), owner);
     }
     
-    /// @dev requires modified function to be called by `owner`
+    /// @dev access control modifier that requires modified function to be called by `owner` account
     modifier onlyOwner {
-        require(msg.sender == owner);
+        require(msg.sender == _owner, "!owner);
         _;
     } 
     
-    /// @dev update `owner` account
-    /// @param _owner account with `onlyOwner` permission 
-    function updateOwner(address _owner) external onlyOwner {
-        owner = _owner;
+    /// @dev return `owner` account with access control
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+    
+    /// @dev return whether caller has access control
+    function isOwner() public view virtual returns (bool) {
+        if (msg.sender == _owner) {
+            return true;
+        }
+    }
+    
+    /// @dev return `pendingOwner` account with access control claim
+    function pendingOwner() public view virtual returns (address) {
+        return _pendingOwner;
+    }
+    
+    /// @dev return whether caller has access control claim
+    function isPendingOwner() public view virtual returns (bool) {
+        if (msg.sender == _pendingOwner) {
+            return true;
+        }
+    }
+    
+    /// @dev `pendingOwner` can claim `owner` account
+    function claimOwner() external {
+        require(msg.sender == _pendingOwner, "!pendingOwner");
+        emit TransferOwnership(_owner, msg.sender);
+        _owner = msg.sender;
+        _pendingOwner = address(0);
+    }
+    
+    /// @dev transfer `owner` account
+    /// @param to Account granted `owner` access control
+    function transferOwnership(address to, bool direct) external onlyOwner {
+        if (direct) {
+            _owner = to;
+            emit TransferOwnership(msg.sender, to);
+        } else {
+            _pendingOwner = to;
+            emit TransferOwnershipClaim(msg.sender, to);
+        }
     }
 }
