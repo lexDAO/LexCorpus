f80e7b58f1f4afd4bcc61ace2453cab112d75412
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-02-18 11:55:16 -0500

    Update and rename contracts/token/lextoken/solidity/Token.sol to contracts/token/StandardToken.sol
    reorg

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -1,13 +1,4 @@
-/*                   
-████████╗ ██████╗ ██╗  ██╗███████╗███╗   ██╗
-╚══██╔══╝██╔═══██╗██║ ██╔╝██╔════╝████╗  ██║
-   ██║   ██║   ██║█████╔╝ █████╗  ██╔██╗ ██║
-   ██║   ██║   ██║██╔═██╗ ██╔══╝  ██║╚██╗██║
-   ██║   ╚██████╔╝██║  ██╗███████╗██║ ╚████║
-   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝
-presented by LexDAO LLC
-*/
-// SPDX-License-Identifier: GPL-3.0-or-later
+/// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.0;
 
 interface IERC20Withdrawal { // brief interface for erc20 token withdrawal
