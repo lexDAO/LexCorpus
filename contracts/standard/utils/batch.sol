d9f1ad3b755ca92926587bc523ef68d3550a4ded
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-05-08 20:20:36 -0400

    Create batch.sol
    simplied form of Boring's https://github.com/boringcrypto/BoringSolidity/blob/master/contracts/BoringBatchable.sol

---
+++ contracts/standard/utils/batch.sol
@@ -0,0 +1,16 @@
+/// Presented by LexDAO LLC
+/// SPDX-License-Identifier: GPL-3.0-or-later
+/// @notice Batch 'multi-call' based on @boringcrypto/boring-solidity/contracts/BoringBatchable.sol@v1.2.0.
+pragma solidity 0.8.4;
+
+contract BatchCall {
+  function batchCall(bytes[] calldata calls, bool revertOnFail) external returns (string memory revertMsg) {
+        for (uint256 i = 0; i < calls.length; i++) {
+            (bool success, bytes memory result) = address(this).delegatecall(calls[i]);
+            if (!success && revertOnFail) 
+                if (result.length < 68) revertMsg = "silent";
+                    assembly {result := add(result, 0x04)}
+                    revertMsg = abi.decode(result, (string)); 
+        }
+    }
+}
