d478975efbaa9c4728d073cac06d0bdae4412fa1
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-05-19 18:40:29 -0400

    Update SafeMath.sol
    fix missing /

--- contracts/utils/libraries/SafeMath.sol
+++ contracts/utils/libraries/SafeMath.sol
@@ -1,7 +1,7 @@
 /// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.6;
 
-// @notice A library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math).
+/// @notice A library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math).
 library SafeMath {
     function add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x, 'SafeMath: Add-Overflow');
