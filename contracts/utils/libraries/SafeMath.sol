64e09d9385600578d0889f6cf72b1f8b718d764d
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-05-19 18:55:05 -0400

    Update SafeMath.sol
    simplify error msg

--- contracts/utils/libraries/SafeMath.sol
+++ contracts/utils/libraries/SafeMath.sol
@@ -4,14 +4,14 @@ pragma solidity 0.7.6;
 /// @notice A library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math).
 library SafeMath {
     function add(uint x, uint y) internal pure returns (uint z) {
-        require((z = x + y) >= x, 'SafeMath: Add-Overflow');
+        require((z = x + y) >= x, 'SafeMath:Add-Over');
     }
 
     function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x, 'SafeMath: Sub-Overflow');
+        require((z = x - y) <= x, 'SafeMath:Sub-Over');
     }
 
     function mul(uint x, uint y) internal pure returns (uint z) {
-        require(y == 0 || (z = x * y) / y == x, 'SafeMath: Mul-Overflow');
+        require(y == 0 || (z = x * y) / y == x, 'SafeMath:Mul-Over');
     }
 }
