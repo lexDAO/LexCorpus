841822c05b305b76692e8a35ce2cb75df6a813f3
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-06-04 16:55:45 -0400

    Create NFT.sol
    add minimalist gasOptimized nft

---
+++ contracts/token/erc721/NFT.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.7.6;
+pragma abicoder v2;
+/// @notice Minimalist NFT minter.
+contract MiniNFT {
+    uint256 public totalSupply;
+    string constant public name = "MiniNFT";
+    string constant public symbol = "mNFT";
+    
+    mapping(address => uint256) public balanceOf;
+    mapping(uint256 => address) public getApproved;
+    mapping(uint256 => address) public ownerOf;
+    mapping(uint256 => string) public tokenURI;
+    mapping(bytes4 => bool) public supportsInterface; // ERC-165 
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
+    
+    event Approval(address indexed approver, address indexed spender, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed approver, address indexed operator, bool approved);
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    constructor() {
+        supportsInterface[0x80ac58cd] = true; // ERC-721 
+        supportsInterface[0x5b5e139f] = true; // METADATA
+    }
+    
+    function approve(address spender, uint256 tokenId) external {
+        address owner = ownerOf[tokenId];
+        require(msg.sender == owner || isApprovedForAll[owner][msg.sender], "!owner/operator");
+        getApproved[tokenId] = spender;
+        emit Approval(msg.sender, spender, tokenId); 
+    }
+    
+    function setApprovalForAll(address operator, bool approved) external {
+        isApprovedForAll[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+    
+    function batch(bytes[] calldata data) external returns (bytes[] memory results) {
+        results = new bytes[](data.length);
+        for (uint256 i = 0; i < data.length; i++) {
+            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+            if (!success) {
+                if (result.length < 68) revert();
+                assembly {result := add(result, 0x04)}
+                revert(abi.decode(result, (string)));
+            }
+            results[i] = result;
+        }
+    }
+    
+    function mint(uint256 tokenId, string calldata _tokenURI) external { 
+        require(ownerOf[tokenId] == address(0), 'tokenId !unique');
+        totalSupply++;
+        balanceOf[msg.sender]++;
+        ownerOf[tokenId] = msg.sender;
+        tokenURI[tokenId] = _tokenURI;
+        emit Transfer(address(0), msg.sender, tokenId); 
+    }
+
+    function transfer(address to, uint256 tokenId) external {
+        require(msg.sender == ownerOf[tokenId], '!owner');
+        balanceOf[msg.sender]--; 
+        balanceOf[to]++; 
+        getApproved[tokenId] = address(0);
+        ownerOf[tokenId] = to;
+        emit Transfer(msg.sender, to, tokenId); 
+    }
+    
+    function transferFrom(address, address to, uint256 tokenId) external {
+        address owner = ownerOf[tokenId];
+        require(msg.sender == owner || getApproved[tokenId] == msg.sender || isApprovedForAll[owner][msg.sender], '!owner/spender/operator');
+        balanceOf[owner]--; 
+        balanceOf[to]++; 
+        getApproved[tokenId] = address(0);
+        ownerOf[tokenId] = to;
+        emit Transfer(owner, to, tokenId); 
+    }
+}
