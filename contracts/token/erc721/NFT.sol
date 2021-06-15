cc28a1b6699226b186bdbb83bb51a8c4ae66cb4f
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-06-15 19:47:26 -0400

    Update NFT.sol
    remove batch per Alberto Cuesta Cañada suggestion to make NFT simpler (folks can use library ~~) https://twitter.com/acuestacanada/status/1404921373961691136?s=20

--- contracts/token/erc721/NFT.sol
+++ contracts/token/erc721/NFT.sol
@@ -32,20 +32,7 @@ contract MiniNFT {
         isApprovedForAll[msg.sender][operator] = approved;
         emit ApprovalForAll(msg.sender, operator, approved);
     }
-    
-    function batch(bytes[] calldata data) external returns (bytes[] memory results) {
-        results = new bytes[](data.length);
-        for (uint256 i = 0; i < data.length; i++) {
-            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
-            if (!success) {
-                if (result.length < 68) revert();
-                assembly {result := add(result, 0x04)}
-                revert(abi.decode(result, (string)));
-            }
-            results[i] = result;
-        }
-    }
-    
+
     function mint(uint256 tokenId, string calldata _tokenURI) external { 
         require(ownerOf[tokenId] == address(0), 'tokenId !unique');
         totalSupply++;
