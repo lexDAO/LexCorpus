5af389f64f0011d959219bdee0a59ebfef6a933c
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-03 20:09:32 -0400

    Update MYSTICSUMMONER.sol

--- contracts/org/molochdao/mystic/MYSTICSUMMONER.sol
+++ contracts/org/molochdao/mystic/MYSTICSUMMONER.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.6.12;
+pragma solidity ^0.8.0;
 
 interface IERC20 { // brief interface for erc20 token tx
     function balanceOf(address account) external view returns (uint256);
