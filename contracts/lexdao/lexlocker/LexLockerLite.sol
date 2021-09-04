898c9dd4c8d967a4b77ba1a2dfa4a326d252adbe
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-03 20:07:34 -0400

    Update LexLockerLite.sol

--- contracts/lexdao/lexlocker/LexLockerLite.sol
+++ contracts/lexdao/lexlocker/LexLockerLite.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.7;
+pragma solidity ^0.8.0;
 /// @notice Bilateral escrow for ETH and ERC20/721.
 /// @author LexDAO LLC.
 contract LexLockerLite {
