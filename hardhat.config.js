0ea5d99425143e24653a4954065a359d500e62cc
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-03 20:00:38 -0400

    Update hardhat.config.js
    bump compilers

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,13 +5,35 @@ require('@nomiclabs/hardhat-waffle');
  */
 module.exports = {
   solidity: {
-    version: '0.8.7',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.4.19",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   networks: {
     hardhat: {
