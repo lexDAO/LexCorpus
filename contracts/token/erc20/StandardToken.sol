7e86e596564bfda68fcbf1a70e6ba34e9277ce31
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-03 20:09:56 -0400

    Update StandardToken.sol

--- contracts/token/erc20/StandardToken.sol
+++ contracts/token/erc20/StandardToken.sol
@@ -1,5 +1,5 @@
 /// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 interface IERC20Withdrawal { // brief interface for erc20 token withdrawal
     function balanceOf(address account) external view returns (uint256);
