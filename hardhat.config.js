f6ebf5e702691c8b180c1efe243bed31df88b28d
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-09-03 20:14:34 -0400

    Update hardhat.config.js
    revert

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,16 +5,14 @@ require('@nomiclabs/hardhat-waffle');
  */
 module.exports = {
   solidity: {
-    compilers: [
-      {
-        version: "^0.8.0",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 99999,
-          },
-        },
-      },
+    version: '^0.8.0',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 283
+      }
+    }
+  },
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true
