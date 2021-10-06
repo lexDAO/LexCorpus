a42b6c7ef467c6dc3ed788b533a58afad9683320
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-06 02:57:12 -0400

    Update and rename StandardToken.sol to LexToken.sol
    adapt solmate for lextoken

---
+++ contracts/token/erc20/LexToken.sol
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+/// @notice Modern and gas efficient ERC20 + EIP-2612 implementation.
+/// @author Adapted from RariCapital, https://github.com/Rari-Capital/solmate/blob/main/src/erc20/ERC20.sol,
+/// License-Identifier: AGPL-3.0-only.
+contract LexToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    string public name;
+    string public symbol;
+
+    uint8 public immutable decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    bytes32 public constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public immutable DOMAIN_SEPARATOR;
+
+    mapping(address => uint256) public nonces;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _supply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes(name)),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
+        
+        _mint(msg.sender, _supply);
+    }
+
+    function approve(address spender, uint256 value) public virtual returns (bool) {
+        allowance[msg.sender][spender] = value;
+
+        emit Approval(msg.sender, spender, value);
+
+        return true;
+    }
+
+    function transfer(address to, uint256 value) public virtual returns (bool) {
+        balanceOf[msg.sender] -= value;
+
+        // This is safe because the sum of all user
+        // balances can't exceed type(uint256).max!
+        unchecked {
+            balanceOf[to] += value;
+        }
+
+        emit Transfer(msg.sender, to, value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public virtual returns (bool) {
+        if (allowance[from][msg.sender] != type(uint256).max) {
+            allowance[from][msg.sender] -= value;
+        }
+
+        balanceOf[from] -= value;
+
+        // This is safe because the sum of all user
+        // balances can't exceed type(uint256).max!
+        unchecked {
+            balanceOf[to] += value;
+        }
+
+        emit Transfer(from, to, value);
+
+        return true;
+    }
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+        require(deadline >= block.timestamp, "PERMIT_DEADLINE_EXPIRED");
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
+            )
+        );
+
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == owner, "INVALID_PERMIT_SIGNATURE");
+
+        allowance[recoveredAddress][spender] = value;
+
+        emit Approval(owner, spender, value);
+    }
+
+    function _mint(address to, uint256 value) internal {
+        totalSupply += value;
+
+        // This is safe because the sum of all user
+        // balances can't exceed type(uint256).max!
+        unchecked {
+            balanceOf[to] += value;
+        }
+
+        emit Transfer(address(0), to, value);
+    }
+
+    function _burn(address from, uint256 value) internal {
+        balanceOf[from] -= value;
+
+        // This is safe because a user won't ever
+        // have a balance larger than totalSupply!
+        unchecked {
+            totalSupply -= value;
+        }
+
+        emit Transfer(from, address(0), value);
+    }
+}
