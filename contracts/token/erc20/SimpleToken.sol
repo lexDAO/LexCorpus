e87024187a7066215e8e19d57cbe4e4c365ccb01
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-06 02:58:29 -0400

    Update SimpleToken.sol
    nits

--- contracts/token/erc20/SimpleToken.sol
+++ contracts/token/erc20/SimpleToken.sol
@@ -1,6 +1,7 @@
-/// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
-/// @notice Basic ERC20 implementation.
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.0;
+
+/// @notice Basic ERC20 token implementation.
 contract Token {
     string public name;
     string public symbol;
