7e2d6e62c5415ecd65152fd64dbb142eac598541
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-08 02:47:37 -0400

    Create LexPausable.sol
    create conditions contract around function pausing

---
+++ contracts/conditions/LexPausable.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.0;
+
+import "./LexOwnable.sol";
+
+/// @notice Function pausing contract.
+abstract contract LexPausable is LexOwnable {
+    bool public paused;
+    
+    /// @notice Initialize contract with `paused` status.
+    constructor(bool _paused) {
+        paused = _paused;
+    }
+    
+    /// @notice Function pausability modifier.
+    modifier notPaused() {
+        require(!paused, "PAUSED");
+        _;
+    }
+    
+    /// @notice Sets function pausing status.
+    /// @param _paused If 'true', modified functions are paused.
+    function setPause(bool _paused) external onlyOwner {
+        paused = _paused;
+    }
+}
