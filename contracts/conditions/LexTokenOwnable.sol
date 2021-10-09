d77027927140c2a851b7f510de5057f02d230173
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-09 15:18:09 -0400

    Update LexTokenOwnable.sol
    conform to other conditions

--- contracts/conditions/LexTokenOwnable.sol
+++ contracts/conditions/LexTokenOwnable.sol
@@ -5,35 +5,40 @@ pragma solidity >=0.8.0;
 import "../interfaces/IERC20.sol";
 import "./LexOwnable.sol";
 
-/// @notice Access control contract based on ERC20/721 balances.
+/// @notice ERC-20/721 balance-based function access control module.
 abstract contract LexTokenOwnable is LexOwnable {
-    event SetOwnerTokenBalance(IERC20 indexed token, uint256 indexed ownerBalance);
+    event UpdateOwnerTokenBalance(IERC20 indexed token, uint256 indexed ownerBalance);
     
     IERC20 public token; 
     uint256 public ownerBalance; 
 
-    /// @notice Initialize contract with `token` and `ownerBalance` variables.
+    /// @notice Initialize token ownership module for function access control.
+    /// @param _token ERC-20/721 token to use for ownership checks.
+    /// @param _ownerBalance Token balance required for ownership access.
     constructor(IERC20 _token, uint256 _ownerBalance) {
         token = _token;
         ownerBalance = _ownerBalance;
-        emit SetOwnerTokenBalance(_token, _ownerBalance);
+        emit UpdateOwnerTokenBalance(_token, _ownerBalance);
     }
     
-    /// @notice Access control modifier that conditions modified function to be called by account with `ownerbalance` of `token`.
+    /// @notice Access control modifier that conditions function to be restricted to account with `ownerbalance` of `token`.
     modifier onlyTokenOwner {
         require(token.balanceOf(msg.sender) >= ownerBalance, "NOT_OWNER");
         _;
     } 
 
-    /// @notice Returns whether `account` has access control based on `token` `ownerBalance`.
+    /// @notice Returns whether `account` has ownership access based on their `token` `ownerBalance`.
+    /// @param account Address to check.
     function checkOwner(address account) external view returns (bool owner) {
         owner = token.balanceOf(account) >= ownerBalance;
     }
 
-    /// @notice Sets `token` and `ownerBalance` for access control.
-    function setOwnerTokenBalance(IERC20 _token, uint256 _ownerBalance) external onlyOwner {
+    /// @notice Update `token` and `ownerBalance` for access control.
+    /// @param _token ERC-20/721 token to use for ownership checks.
+    /// @param _ownerBalance Token balance required for ownership access.
+    function updateOwnerTokenBalance(IERC20 _token, uint256 _ownerBalance) external onlyOwner {
         token = _token;
         ownerBalance = _ownerBalance;
-        emit SetOwnerTokenBalance(_token, _ownerBalance);
+        emit UpdateOwnerTokenBalance(_token, _ownerBalance);
     }
 }
