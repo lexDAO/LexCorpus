bedca2110454c4e6f95d0f74f2b965eb86ff3fe2
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-11 01:23:35 -0400

    Update Certification.sol
    match deploy

--- contracts/lexdao/certification/Certification.sol
+++ contracts/lexdao/certification/Certification.sol
@@ -1,7 +1,7 @@
+/// SPDX-License-Identifier: MIT
 /// Presented by LexDAO LLC
-/// SPDX-License-Identifier: GPL-3.0-or-later
 /// @notice Minimal Certification NFT.
-pragma solidity ^0.8.0;
+pragma solidity 0.8.4;
 
 contract Certification {
     address public governance;
@@ -17,8 +17,9 @@ contract Certification {
     mapping(bytes4 => bool) public supportsInterface; // ERC-165 
     
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event TransferGovernance(address indexed governance, string details);
     event GovTokenURI(uint256 indexed tokenId, string tokenURI);
+    event TransferGovernance(address indexed governance);
+    event UpdateBaseURI(string baseURI);
     
     constructor(
         address _governance,
@@ -35,33 +36,10 @@ contract Certification {
         supportsInterface[0x80ac58cd] = true; // ERC-721 
         supportsInterface[0x5b5e139f] = true; // METADATA
     }
-    
-    function batchCall(bytes[] calldata calls, bool revertOnFail) external returns (string memory revertMsg) {
-        for (uint256 i = 0; i < calls.length; i++) {
-            (bool success, bytes memory result) = address(this).delegatecall(calls[i]);
-            if (!success && revertOnFail) 
-                if (result.length < 68) revertMsg = 'silent';
-                    assembly {result := add(result, 0x04)}
-                    revertMsg = abi.decode(result, (string)); 
-        }
-    }
-    
-    /// **** GOVERNANCE
+
     modifier onlyGovernance {
         require(msg.sender == governance, '!governance');
         _;
-    }
-    
-    function mint(address to, string calldata customURI) external onlyGovernance { 
-        string memory _tokenURI; 
-        bytes(customURI).length > 0 ? _tokenURI = customURI : _tokenURI = baseURI;
-        totalSupply++;
-        uint256 tokenId = totalSupply;
-        balanceOf[to]++;
-        registration[to] = block.timestamp;
-        ownerOf[tokenId] = to;
-        tokenURI[tokenId] = _tokenURI;
-        emit Transfer(address(0), to, tokenId);
     }
     
     function burn(address from, uint256 tokenId) external {
@@ -72,6 +50,17 @@ contract Certification {
         emit Transfer(from, address(0), tokenId); 
     }
     
+    function mint(address to, string calldata customURI) external onlyGovernance { 
+        string memory _tokenURI; 
+        bytes(customURI).length > 0 ? _tokenURI = customURI : _tokenURI = baseURI;
+        totalSupply++;
+        uint256 tokenId = totalSupply;
+        balanceOf[to]++;
+        ownerOf[tokenId] = to;
+        tokenURI[tokenId] = _tokenURI;
+        emit Transfer(address(0), to, tokenId);
+    }
+
     function govTokenURI(uint256 tokenId, string calldata _tokenURI) external onlyGovernance {
         require(tokenId <= totalSupply, '!exist');
         tokenURI[tokenId] = _tokenURI;
@@ -86,29 +75,13 @@ contract Certification {
         emit Transfer(from, to, tokenId); 
     }
 
-    function transferGovernance(address _governance, string calldata _details) external onlyGovernance {
+    function transferGovernance(address _governance) external onlyGovernance {
         governance = _governance;
-        details = _details;
-        emit TransferGovernance(_governance, _details);
+        emit TransferGovernance(_governance);
     }
-}
-
-contract CertificationFactory {
-    event DeployCertification(Certification indexed certification, address indexed governance);
     
-    function deployCertification(
-        address _governance, 
-        string memory _baseURI, 
-        string memory _details, 
-        string memory _name, 
-        string memory _symbol
-    ) external returns (Certification certification) {
-        certification = new Certification(
-            _governance, 
-            _baseURI, 
-            _details, 
-            _name, 
-            _symbol);
-        emit DeployCertification(certification, _governance);
+    function updateBaseURI(string calldata _baseURI) external onlyGovernance {
+        baseURI = _baseURI;
+        emit UpdateBaseURI(_baseURI);
     }
 }
