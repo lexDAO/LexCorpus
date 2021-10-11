1fa330afa773954a479c72eb5a1b862b73936fe1
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-11 01:36:10 -0400

    Update and rename LivingTrustV1.sol to LivingTrust.sol
    nits

--- contracts/lexdao/trust/LivingTrust.sol
+++ contracts/lexdao/trust/LivingTrust.sol
@@ -1,23 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 pragma solidity >=0.8.0;
 
-/// @notice Standard ERC-20 token interface with EIP-2612 {permit} extension.
-interface IERC20 { 
-    /// @dev ERC-20:
-    function allowance(address owner, address spender) external view returns (uint);
-    function balanceOf(address account) external view returns (uint);
-    function totalSupply() external view returns (uint);
-    function approve(address spender, uint amount) external returns (bool);
-    function transfer(address to, uint amount) external returns (bool);
-    function transferFrom(address from, address to, uint amount) external returns (bool);
-    event Approval(address indexed owner, address indexed spender, uint amount);
-    event Transfer(address indexed from, address indexed to, uint amount);
-    /// @dev EIP-2612:
-    function permit(address owner, address spender, uint amount, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
-}
+import "../../interfaces/IERC20.sol";
 
-/// @dev Hacked by LexDAO LLC.
 contract LivingTrustV1 {
     address public grantor; // supplies funds to vault - initial trustee
     address public beneficiary; // can claim trust funds - initially grantor
