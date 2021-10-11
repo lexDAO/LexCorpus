5cbb9e91c803813a059a72ebcb88ce060589083a
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-11 01:34:00 -0400

    Update SelfRegOrg.sol
    nit

--- contracts/lexdao/SelfRegOrg.sol
+++ contracts/lexdao/SelfRegOrg.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0;
 
-import "../conditions/Ownable.sol";
+import "../conditions/LexOwnable.sol";
 
 contract SelfRegOrg is Ownable {
     string public purposeAndRules;
