387818e12e7195360d355284b6d6fe09f497d6ca
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-11 23:38:16 -0400

    Update LexNFTIncrementalMintableOwnable.sol
    elaborate notes and add gas optimization

--- contracts/token/erc721/extensions/LexNFTIncrementalMintableOwnable.sol
+++ contracts/token/erc721/extensions/LexNFTIncrementalMintableOwnable.sol
@@ -7,13 +7,21 @@ import '../../../LexOwnable.sol';
 
 /// @notice Extension for LexNFT that allows owner-restricted (incremental) minting and burning.
 contract LexNFTIncrementalMintableOwnable is LexNFT, LexOwnable {
+    /// @notice Initialize LexNFT extension.
+    /// @param _name Public name for LexNFT.
+    /// @param _symbol Public symbol for LexNFT.
+    /// @param _owner Account to grant minting ownership. 
     constructor(string memory _name, string memory _symbol, address _owner) 
         LexNFT(_name, _symbol) LexOwnable(_owner) {}
     
     /// @notice Mints a new `tokenId` for `to` incremented from `totalSupply`.
     /// @param to Account that receives mint.
     /// @param _tokenURI MetaData to attach to mint.
-    function mint(address to, string memory _tokenURI) public onlyOwner {
-        _mint(to, totalSupply + 1, _tokenURI);
+    function mint(address to, string memory _tokenURI) external onlyOwner {
+        /// @dev This is reasonably safe from overflow because incrementing `totalSupply` beyond
+        // 'type(uint256).max' is exceedingly unlikely compared to optimization benefits.
+        unchecked {
+            _mint(to, totalSupply + 1, _tokenURI);
+        }
     }
 }
