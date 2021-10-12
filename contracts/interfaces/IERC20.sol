593b5a95169811a35291e72b440a76789a2c7eb1
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-11 23:00:24 -0400

    Update IERC20.sol
    style nit (sigh)

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,5 +1,6 @@
-/// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.7.0;
 
 /// @notice Standard ERC-20 token interface with EIP-2612 {permit} extension.
 interface IERC20 { 
