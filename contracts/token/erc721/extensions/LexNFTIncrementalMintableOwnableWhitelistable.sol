4fb8ce75828d0af4b2a5f6965fb9e0143fdffd3d
Jordan Teague <ejteague@gmail.com>
2021-10-23 14:28:36 -0400

    Create LexNFTIncrementalMintableOwnableWhitelistable.sol
    preset for incremental, mintable, ownable, whitelistable (merkle root)

---
+++ contracts/token/erc721/extensions/LexNFTIncrementalMintableOwnableWhitelistable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.0;
+
+import '../LexNFT.sol';
+import '../../../conditions/LexOwnable.sol';
+import '../../../conditions/LexWhiteListableMerkleRoot.sol';
+
+/// @notice Extension for LexNFT that allows owner-restricted (incremental) minting and burning, and whitelisting via Merkle root.
+contract LexNFTIncrementalMintableOwnable is LexNFT, LexWhiteListableMerkle {
+    /// @notice Initialize LexNFT extension.
+    /// @param _name Public name for LexNFT.
+    /// @param _symbol Public symbol for LexNFT.
+    /// @param _owner Account to grant minting ownership.
+    constructor(string memory _name, string memory _symbol, address _owner, bool _whitelistenabled)
+        LexNFT(_name, _symbol) LexWhiteListableMerkle(_whitelistenabled, _owner) {}
+
+    /// @notice Mints a new `tokenId` for `to` incremented from `totalSupply`.
+    /// @param to Account that receives mint.
+    /// @param _tokenURI MetaData to attach to mint.
+    function mint(address to, string memory _tokenURI) external onlyOwner {
+        /// @dev This is reasonably safe from overflow because incrementing `totalSupply` beyond
+        // 'type(uint256).max' is exceedingly unlikely compared to optimization benefits.
+        unchecked {
+            _mint(to, totalSupply + 1, _tokenURI);
+        }
+    }
+}
