6a4113dc85dcb5f961db061266439b812e956340
Jordan Teague <ejteague@gmail.com>
2021-10-29 13:41:58 -0400

    Update LexNFTIncrementalMintableOwnable.sol
    corrected file path for LexNFT

--- contracts/token/erc721/extensions/LexNFTIncrementalMintableOwnable.sol
+++ contracts/token/erc721/extensions/LexNFTIncrementalMintableOwnable.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.8.0;
 
-import '../../../LexNFT.sol';
+import '../LexNFT.sol';
 import '../../../LexOwnable.sol';
 
 /// @notice Extension for LexNFT that allows owner-restricted (incremental) minting and burning.
