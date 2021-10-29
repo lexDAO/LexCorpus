1c5e23444443372889db4f05cb82bbac2970076a
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-10-29 13:29:24 -0400

    Update LexNFT.sol (#48)
    domainSeparator typo fixes

--- contracts/token/erc721/LexNFT.sol
+++ contracts/token/erc721/LexNFT.sol
@@ -42,8 +42,8 @@ abstract contract LexNFT {
         INITIAL_DOMAIN_SEPARATOR = _calculateDomainSeparator();
     }
     
-    function _calculateDomainSeparator() internal view returns (bytes32 domainSeperator) {
-        domainSeperator = keccak256(
+    function _calculateDomainSeparator() internal view returns (bytes32 domainSeparator) {
+        domainSeparator = keccak256(
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
                 keccak256(bytes(name)),
@@ -54,8 +54,8 @@ abstract contract LexNFT {
         );
     }
     
-    function DOMAIN_SEPARATOR() public view returns (bytes32 domainSeperator) {
-        domainSeperator = block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : _calculateDomainSeparator();
+    function DOMAIN_SEPARATOR() public view returns (bytes32 domainSeparator) {
+        domainSeparator = block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : _calculateDomainSeparator();
     }
     
     function supportsInterface(bytes4 interfaceId) public pure returns (bool supported) {
