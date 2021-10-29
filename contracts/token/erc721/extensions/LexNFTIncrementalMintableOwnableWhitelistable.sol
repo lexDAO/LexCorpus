2eddbc56c85e63b946f3ac95ee798529d0cef9c0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-29 13:33:00 -0400

    Update LexNFTIncrementalMintableOwnableWhitelistable.sol
    nit var and comments

--- contracts/token/erc721/extensions/LexNFTIncrementalMintableOwnableWhitelistable.sol
+++ contracts/token/erc721/extensions/LexNFTIncrementalMintableOwnableWhitelistable.sol
@@ -12,14 +12,14 @@ contract LexNFTIncrementalMintableOwnable is LexNFT, LexWhiteListableMerkle {
     /// @param _name Public name for LexNFT.
     /// @param _symbol Public symbol for LexNFT.
     /// @param _owner Account to grant minting ownership.
-    constructor(string memory _name, string memory _symbol, address _owner, bool _whitelistenabled)
-        LexNFT(_name, _symbol) LexWhiteListableMerkle(_whitelistenabled, _owner) {}
+    constructor(string memory _name, string memory _symbol, address _owner, bool _whitelistEnabled)
+        LexNFT(_name, _symbol) LexWhiteListableMerkle(_whitelistEnabled, _owner) {}
 
     /// @notice Mints a new `tokenId` for `to` incremented from `totalSupply`.
     /// @param to Account that receives mint.
     /// @param _tokenURI MetaData to attach to mint.
     function mint(address to, string memory _tokenURI) external onlyOwner {
-        /// @dev This is reasonably safe from overflow because incrementing `totalSupply` beyond
+        // This is reasonably safe from overflow because incrementing `totalSupply` beyond
         // 'type(uint256).max' is exceedingly unlikely compared to optimization benefits.
         unchecked {
             _mint(to, totalSupply + 1, _tokenURI);
