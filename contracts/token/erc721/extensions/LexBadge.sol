5ea6aba5da145a969390e93226be911566414df3
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-29 13:49:58 -0400

    Update LexBadge.sol
    nits on comments and vars

--- contracts/token/erc721/extensions/LexBadge.sol
+++ contracts/token/erc721/extensions/LexBadge.sol
@@ -13,33 +13,37 @@ contract LexBadge is LexNFT, LexOwnable {
     /// @param _owner Account to grant minting ownership.
     constructor(string memory _name, string memory _symbol, address _owner) LexNFT(_name, _symbol) LexOwnable(_owner) {}
 
-    /// @notice Disables transferability by overriding transfer functions of LexNFT.
+    /// @notice Disables transferability by overriding {transfer} function of LexNFT.
     function transfer(address, uint256) public pure override {
         revert();
     }
-
+    
+    /// @notice Disables transferability by overriding {transferFrom} function of LexNFT.
     function transferFrom(address, address, uint256) public pure override {
         revert();
     }
-
+    
+    /// @notice Disables transferability by overriding {safeTransferFrom} function of LexNFT.
     function safeTransferFrom(address, address, uint256) public pure override {
         revert();
     }
-
+    
+    /// @notice Disables transferability by overriding {safeTransferFrom} function of LexNFT.
     function safeTransferFrom(address, address, uint256, bytes calldata) public pure override {
         revert();
     }
 
     /// @notice Mints a new token at `tokenId` for `to`.
     /// @param to Account that receives mint.
-    /// @param _tokenURI MetaData to attach to mint.
-    function mint(address to, uint256 tokenId, string memory _tokenURI) external onlyOwner {
-        _mint(to, tokenId, _tokenURI);
+    /// @param tokenId # to attach to mint.
+    /// @param tokenURI MetaData to attach to mint.
+    function mint(address to, uint256 tokenId, string memory tokenURI) external onlyOwner {
+        _mint(to, tokenId, tokenURI);
     }
 
     /// @notice Burns token at `tokenId`.
-    function burn(uint tokenId) external onlyOwner {
+    /// @param tokenId # to attach to mint.
+    function burn(uint256 tokenId) external onlyOwner {
         _burn(tokenId);
     }
-
 }
