80fbd53e19d3ba2df8c5063046ae35248fb16c45
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-10-30 11:27:59 -0400

    Update LexTokenMintable.sol
    add token initial supply param and minting

--- contracts/token/erc20/extensions/LexTokenMintable.sol
+++ contracts/token/erc20/extensions/LexTokenMintable.sol
@@ -11,8 +11,17 @@ contract LexTokenMintable is LexToken, LexOwnable {
     /// @param _name Public name for LexToken.
     /// @param _symbol Public symbol for LexToken.
     /// @param _decimals Unit scaling factor - default '18' to match ETH units. 
-    /// @param _owner Account to grant minting ownership.
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, address _owner) LexToken(_name, _symbol, _decimals) LexOwnable(_owner) {}
+    /// @param _owner Account to grant minting `owner` access role.
+    /// @param _initialSupply Starting LexToken supply to mint to `owner`.
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint8 _decimals, 
+        address _owner,
+        uint256 _initialSupply
+    ) LexToken(_name, _symbol, _decimals) LexOwnable(_owner) {
+        _mint(_owner, _initialSupply);
+    }
     
     /// @notice Mints tokens by `owner`.
     /// @param to Account to receive tokens.
