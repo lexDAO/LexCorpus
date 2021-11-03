f873c75807164a4c92f937406f1e1d042e241df4
z0r0z <92001561+z0r0z@users.noreply.github.com>
2021-11-03 17:19:58 -0400

    Update LexRegistry.sol
    nits: put `lawyer` param into struct mapping / typographic changes

--- contracts/lexdao/LexRegistry.sol
+++ contracts/lexdao/LexRegistry.sol
@@ -1,39 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
 pragma solidity >=0.8.0;
 
+/// @notice A simple lawyer registry contract for LexDAO.
 contract LexRegistry {
-    
     address public owner;
-    
+
     mapping(address => Lawyer) public lawyers;
-    
+
     struct Lawyer {
         string name;
         string jurisdiction;
         string details;
         bool availability;
     }
-    
-    constructor()  {
+
+    constructor() {
         owner = msg.sender;
     }
-    
+
     function register(address lawyer, string calldata name, string calldata jurisdiction, string calldata details) external {
         require(msg.sender == owner, "NOT_OWNER");
-        lawyers[owner] = Lawyer(name, jurisdiction, details, true);
-    
+        lawyers[lawyer] = Lawyer(name, jurisdiction, details, true);
     }
-    
+
     function updateAvailability(bool availability) external {
         bytes memory name = bytes(lawyers[msg.sender].name);
         require(name.length > 0, "NOT_LAWYER");
         lawyers[msg.sender].availability = availability;
     }
 
-    function transferOwner(address _owner) external {
+    function transferOwner(address owner_) external {
         require(msg.sender == owner, "NOT_OWNER");
-        owner = _owner;
+        owner = owner_;
     }
 }
-
-
