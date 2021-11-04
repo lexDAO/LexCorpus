5b87b4a72775a9f6364e1204d1664da81db2b1db
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-11-04 10:19:59 -0400

    Update LexRegistry.sol
    adding some events and comments to explain the register() params

--- contracts/lexdao/LexRegistry.sol
+++ contracts/lexdao/LexRegistry.sol
@@ -14,20 +14,29 @@ contract LexRegistry {
         string details;
         bool availability;
     }
+    
+    event LawyerRegistered(address indexed lawyer, string name, string jurisdiction, string details);
+    event UpdatedAvailability(address indexed lawyer, bool availability);
 
     constructor() {
         owner = msg.sender;
     }
-
+    
+    // @param lawyer: ETH address of lawyer to be registered
+    // @param name: name of lawyer
+    // @param jurisdiction: jurisdiction(s) in which lawyer is qualified to practice 
+    // @param details: lawyer's link, bio, or concise explanation of lawyer's expertise or experience
     function register(address lawyer, string calldata name, string calldata jurisdiction, string calldata details) external {
         require(msg.sender == owner, "NOT_OWNER");
         lawyers[lawyer] = Lawyer(name, jurisdiction, details, true);
+        emit LawyerRegistered(lawyer, name, jurisdiction, details);
     }
 
     function updateAvailability(bool availability) external {
         bytes memory name = bytes(lawyers[msg.sender].name);
         require(name.length > 0, "NOT_LAWYER");
         lawyers[msg.sender].availability = availability;
+        emit UpdatedAvailability(msg.sender, availability);
     }
 
     function transferOwner(address owner_) external {
